af852d97881f69ba19bedc692f7c48e210a2ba12
zedparman <ze.dparman@gmail.com>
2025-07-19 05:10:59 -0700

    Delete fix-medicines-table.sql

--- fix-medicines-table.sql
+++
@@ -1,9 +0,0 @@
--- اضافه کردن ستون image_url به جدول medicines
-ALTER TABLE medicines 
-ADD COLUMN IF NOT EXISTS image_url TEXT;
-
--- نمایش ساختار جدول
-SELECT column_name, data_type, is_nullable, column_default
-FROM information_schema.columns 
-WHERE table_name = 'medicines' 
-ORDER BY ordinal_position; 
