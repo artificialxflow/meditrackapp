66c17daecadcf85bbf00ac19064d82ea61b5245b
zedparman <ze.dparman@gmail.com>
2025-07-19 04:48:13 -0700

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@
 import Link from 'next/link'
 import { useAuth } from '@/hooks/useAuth'
 import { useTheme } from '@/providers/ThemeProvider'
-import { FaSun, FaMoon } from 'react-icons/fa'
+import { FaSun, FaMoon, FaHome, FaUserInjured, FaUsers, FaPills, FaCalendarAlt, FaHeartbeat, FaFileAlt, FaBell, FaUser, FaQuestionCircle, FaTachometerAlt, FaClock } from 'react-icons/fa'
 
 export default function Navbar() {
   const { user, logout } = useAuth()
@@ -28,50 +28,100 @@ export default function Navbar() {
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            {/* صفحه اصلی */}
+            <li className="nav-item">
+              <Link href="/" className="nav-link d-flex align-items-center">
+                <FaHome className="ms-2" />
+                خانه
+              </Link>
+            </li>
+            
+            {/* راهنما - همیشه قابل دسترس */}
+            <li className="nav-item">
+              <Link href="/guide" className="nav-link d-flex align-items-center">
+                <FaQuestionCircle className="ms-2" />
+                راهنما
+              </Link>
+            </li>
+            
             {user && (
               <>
+                {/* داشبورد - مرکز کنترل */}
                 <li className="nav-item">
-                  <Link href="/dashboard" className="nav-link">
+                  <Link href="/dashboard" className="nav-link d-flex align-items-center">
+                    <FaTachometerAlt className="ms-2" />
                     داشبورد
                   </Link>
                 </li>
+                
+                {/* مدیریت بیماران */}
                 <li className="nav-item">
-                  <Link href="/patients" className="nav-link">
+                  <Link href="/patients" className="nav-link d-flex align-items-center">
+                    <FaUserInjured className="ms-2" />
                     بیماران
                   </Link>
                 </li>
+                
+                {/* مدیریت خانواده‌ها */}
                 <li className="nav-item">
-                  <Link href="/families" className="nav-link">
+                  <Link href="/families" className="nav-link d-flex align-items-center">
+                    <FaUsers className="ms-2" />
                     خانواده‌ها
                   </Link>
                 </li>
+                
+                {/* مدیریت داروها */}
                 <li className="nav-item">
-                  <Link href="/medicines" className="nav-link">
+                  <Link href="/medicines" className="nav-link d-flex align-items-center">
+                    <FaPills className="ms-2" />
                     داروها
                   </Link>
                 </li>
+                
+                {/* برنامه‌های مصرف دارو */}
                 <li className="nav-item">
-                  <Link href="/appointments" className="nav-link">
+                  <Link href="/schedules" className="nav-link d-flex align-items-center">
+                    <FaClock className="ms-2" />
+                    برنامه‌ها
+                  </Link>
+                </li>
+                
+                {/* قرار ملاقات‌ها */}
+                <li className="nav-item">
+                  <Link href="/appointments" className="nav-link d-flex align-items-center">
+                    <FaCalendarAlt className="ms-2" />
                     قرار ملاقات
                   </Link>
                 </li>
+                
+                {/* علائم حیاتی */}
                 <li className="nav-item">
-                  <Link href="/vitals" className="nav-link">
+                  <Link href="/vitals" className="nav-link d-flex align-items-center">
+                    <FaHeartbeat className="ms-2" />
                     علائم حیاتی
                   </Link>
                 </li>
+                
+                {/* اسناد پزشکی */}
                 <li className="nav-item">
-                  <Link href="/documents" className="nav-link">
+                  <Link href="/documents" className="nav-link d-flex align-items-center">
+                    <FaFileAlt className="ms-2" />
                     اسناد
                   </Link>
                 </li>
+                
+                {/* اعلان‌ها */}
                 <li className="nav-item">
-                  <Link href="/notifications" className="nav-link">
+                  <Link href="/notifications" className="nav-link d-flex align-items-center">
+                    <FaBell className="ms-2" />
                     اعلان‌ها
                   </Link>
                 </li>
+                
+                {/* پروفایل کاربر */}
                 <li className="nav-item">
-                  <Link href="/profile" className="nav-link">
+                  <Link href="/profile" className="nav-link d-flex align-items-center">
+                    <FaUser className="ms-2" />
                     پروفایل
                   </Link>
                 </li>
